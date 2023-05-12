d1618295bb46d815084b461994b0a52893345ff1
Douglas de Freitas <douglasmfreitas@gmail.com>
2023-05-12 20:29:45 -0300

    make the time more readable

--- src/components/content/Skill.jsx
+++ src/components/content/Skill.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import './Skill.css'
 
 import ProgressBar from './ProgressBar'
-import Tag from './Tag'
 
 export default function Skill({name, current, max}) {
   return (
     <div class="skill-root">
       <div class="skill-container">
         <div>{name}</div>
-        <div><Tag>{current} YEARS</Tag></div>
+        <div>{current} years</div>
       </div>
       <ProgressBar progress={current * 100.0 / max}/>
     </div>
