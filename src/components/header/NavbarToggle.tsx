e8338bd510b3aa04719dda0b0398fae5a19f3d0d
Douglas de Freitas <douglasmfreitas@gmail.com>
2025-05-26 01:15:39 -0300

    fix navbar on small displays

--- src/components/header/NavbarToggle.tsx
+++ src/components/header/NavbarToggle.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './NavbarToggle.css'
+import { Menu } from 'lucide-react'
 
 interface NavbarToggleProps {
   onToggle: () => void
@@ -9,7 +10,7 @@ export default function NavbarToggle({ onToggle }: NavbarToggleProps) {
   return (
     <div className="navbar-toggle">
       <button onClick={onToggle} className="navbar-toggle-link">
-        <i className="fa fa-bars"></i>
+        <Menu size={36} display='block' />
       </button>
     </div>
   )
